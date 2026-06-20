80370b45f76f4132a72b5d16b04653c505e23cd3
Matthias Voigt <m.voigt@code-ix.de>
2026-06-21 00:11:50 +0200

    UI Improvement

---
+++ src/WhipRadio.Web/Components/UiPrimitives.cs
@@ -0,0 +1,21 @@
+namespace WhipRadio.Web.Components;
+
+public enum SwitchSize
+{
+    Normal,
+    Big,
+}
+
+public enum SwitchTone
+{
+    Amber,
+    Red,
+}
+
+public enum StatusBadgeTone
+{
+    Default,
+    Green,
+    Amber,
+    Red,
+}
